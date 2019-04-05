f2097482a9e7db4bad6538744316a60286e7dc8d
SarawutSukpot <srwspt.boat@hotmail.com>
2019-04-05 15:00:31 +0700

    ooad project version 3.0.0

--- routes/roomRouter.js
+++ routes/roomRouter.js
@@ -35,20 +35,63 @@ Router.route('/createAddMoreRoomx').get(function (req, res) {
 });
 
 Router.route('/create').post(function (req, res) {
-    const DataUser = new Room(req.body);
-    console.log(DataUser)
-    // console.log(buildID)
-    DataUser.save()
-    res.redirect('/home/room')
-
+    var Data = Room(req.body)
+    var buildID = req.body.buildID;
+    Room.findOne({ buildID: buildID }, function (err, room) {
+        if (room) {
+            
+            if (addRoom == 0) {
+                room.roomID.push(req.body.roomID);
+                room.type.push(req.body.type);
+                room.maxStudent.push(parseInt(req.body.maxStudent));
+                room.status.push(req.body.status);
+            } else {
+                for (let i = 0; i <= addRoom; i++) {
+                    room.roomID.push(req.body.roomID[i]);
+                    room.type.push(req.body.type[i]);
+                    room.maxStudent.push(parseInt(req.body.maxStudent[i]));
+                    room.status.push(req.body.status[i]);
+                }
+            }
+            console.log(room)
+            room.save()
+            res.redirect('/home/room')
+        } else {
+            console.log(Data)
+            Data.save()
+            res.redirect('/home/room')
+        }
+    });
 });
 
-Router.route('/delete/:id').get(function (req, res) {
-    Build.findByIdAndRemove({ _id: req.params.id },
-        function (err, coin) {
-            if (err) res.json(err);
-            else res.redirect('/home/build');
-        });
+Router.route('/delete/:id/:roomID').get(function (req, res) {
+    const id = req.params.id;
+    const removeRoomID = req.params.roomID;
+    // console.log(removeRoomID)
+    Room.find({_id:id , roomID:removeRoomID}, function (err, room) {
+       
+        var size = room[0].roomID.length;
+        // console.log(size)
+        if(size == 1){
+            Room.findByIdAndRemove({ _id: id},
+                function (err, room) {
+                    res.redirect('/home/room');
+                });
+        }else{
+            for (let i = 0; i < size; i++) {
+                if(room[0].roomID[i] == removeRoomID){
+                    room[0].roomID.splice(i,i);
+                    room[0].type.splice(i,i);
+                    room[0].maxStudent.splice(i,i);
+                    room[0].status.splice(i,i);
+                }
+            }
+            // console.log(room[0])
+            room[0].save()
+            res.redirect('/home/room');
+        }
+        
+    });
 });
 
 Router.route('/edit/:id').get(function (req, res) {
